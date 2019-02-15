2c8796bc038c663e300af7f798a3ba9021ad6d2f
necraidan <benjamin.auzanneau.tm@gmail.com>
2019-02-15 11:52:30 +0100

    Add first version of player

--- client/src/app/player/player.component.ts
+++ client/src/app/player/player.component.ts
@@ -7,20 +7,46 @@ import { fromEvent } from 'rxjs';
   styleUrls: ['./player.component.scss']
 })
 export class PlayerComponent implements OnInit {
-  @ViewChild('audio')
-  audioElement: ElementRef;
+  currentTime: number;
+  duration: number;
+
+  // @ViewChild('audio')
+  // audioElement: ElementRef;
+  audioElement: HTMLAudioElement;
+
+  @ViewChild('progress')
+  progress: ElementRef;
 
   constructor() {}
 
   ngOnInit() {
+    this.audioElement = new Audio('assets/music/Jazz-Mezzo.mp3');
     console.log(this.audioElement);
-    this.audioElement.nativeElement.play();
 
-    fromEvent(this.audioElement.nativeElement, 'timeupdate').subscribe((res: Event) => {
-      console.log(res);
-      console.log(this.audioElement.nativeElement.currentTime);
-      console.log(this.audioElement.nativeElement.duration);
-      console.log((this.audioElement.nativeElement.currentTime * 100) / this.audioElement.nativeElement.duration);
+    fromEvent(this.audioElement, 'timeupdate').subscribe((res: Event) => {
+      this.currentTime = this.audioElement.currentTime;
+      this.progress.nativeElement.value = (this.audioElement.currentTime * 100) / this.audioElement.duration;
     });
+
+    fromEvent(this.audioElement, 'canplaythrough').subscribe((res: Event) => {
+      this.currentTime = this.audioElement.currentTime;
+      this.duration = this.audioElement.duration;
+    });
+  }
+
+  click(event: MouseEvent) {
+    let x = event.pageX - this.progress.nativeElement.offsetLeft, // or e.offsetX (less support, though)
+      y = event.pageY - this.progress.nativeElement.offsetTop, // or e.offsetY
+      clickedValue = (x * this.progress.nativeElement.max) / this.progress.nativeElement.offsetWidth;
+
+    this.audioElement.currentTime = (this.audioElement.duration * clickedValue) / 100;
+  }
+
+  play() {
+    this.audioElement.play();
+  }
+
+  pause() {
+    this.audioElement.pause();
   }
 }
