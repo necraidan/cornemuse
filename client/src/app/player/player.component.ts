728e96583ea1c383609184b0dca8555e119504c4
BuildTools <benjamin.auzanneau.tm@gmail.com>
2019-02-15 01:36:01 +0100

    Calculate percent for duration and currentTime

--- client/src/app/player/player.component.ts
+++ client/src/app/player/player.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'player',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player.component.scss']
 })
 export class PlayerComponent implements OnInit {
+  @ViewChild('audio')
+  audioElement: ElementRef;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(this.audioElement);
+    this.audioElement.nativeElement.play();
+
+    fromEvent(this.audioElement.nativeElement, 'timeupdate').subscribe((res: Event) => {
+      console.log(res);
+      console.log(this.audioElement.nativeElement.currentTime);
+      console.log(this.audioElement.nativeElement.duration);
+      console.log((this.audioElement.nativeElement.currentTime * 100) / this.audioElement.nativeElement.duration);
+    });
+  }
 }
