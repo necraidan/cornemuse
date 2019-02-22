f06688ebacb80885e3895eca08ed126326063b15
BuildTools <benjamin.auzanneau.tm@gmail.com>
2019-02-22 01:14:05 +0100

    Fix event handler

--- client/src/app/bottom-bar/player/player.component.ts
+++ client/src/app/bottom-bar/player/player.component.ts
@@ -17,33 +17,36 @@ export class PlayerComponent implements OnInit {
 
   @ViewChild('progress')
   progress: ElementRef;
+  // FIX: Types
+  canPlay: any;
+  timeUpdate: any;
 
   constructor(private stateService: StateService) {}
 
   ngOnInit() {
     this.stateService.musicPlaying.subscribe((elt: any) => {
       if (elt) {
+        this.beforeNewMusic();
         this.audioElement = new Audio('http://localhost:4200/api/music/' + elt);
 
-        fromEvent(this.audioElement, 'canplaythrough').subscribe((res: Event) => {
+        this.canPlay = fromEvent(this.audioElement, 'canplaythrough').subscribe((res: Event) => {
+          console.log('loaded !');
           this.currentTime = this.audioElement.currentTime;
           this.duration = this.audioElement.duration;
-
-          fromEvent(this.audioElement, 'timeupdate').subscribe((res: Event) => {
-            this.currentTime = this.audioElement.currentTime;
-            this.progress.nativeElement.value = (this.audioElement.currentTime * 100) / this.audioElement.duration;
-          });
         });
 
-        console.log(this.audioElement);
+        this.timeUpdate = fromEvent(this.audioElement, 'timeupdate').subscribe((res: Event) => {
+          this.currentTime = this.audioElement.currentTime;
+          this.progress.nativeElement.value = (this.audioElement.currentTime * 100) / this.audioElement.duration;
+        });
       }
     });
   }
 
   click(event: MouseEvent) {
-    let x = event.pageX - this.progress.nativeElement.offsetLeft, // or e.offsetX (less support, though)
-      y = event.pageY - this.progress.nativeElement.offsetTop, // or e.offsetY
-      clickedValue = (x * this.progress.nativeElement.max) / this.progress.nativeElement.offsetWidth;
+    const x = event.pageX - this.progress.nativeElement.offsetLeft; // or e.offsetX (less support, though)
+    const y = event.pageY - this.progress.nativeElement.offsetTop; // or e.offsetY
+    const clickedValue = (x * this.progress.nativeElement.max) / this.progress.nativeElement.offsetWidth;
 
     this.audioElement.currentTime = (this.audioElement.duration * clickedValue) / 100;
   }
@@ -54,5 +57,14 @@ export class PlayerComponent implements OnInit {
 
   pause() {
     this.audioElement.pause();
+  }
+
+  beforeNewMusic() {
+    // tslint:disable-next-line:no-unused-expression
+    this.canPlay && this.canPlay.unsubscribe();
+    // tslint:disable-next-line: no-unused-expression
+    this.timeUpdate && this.timeUpdate.unsubscribe();
+    // tslint:disable-next-line: no-unused-expression
+    this.audioElement && this.audioElement.pause();
   }
 }
